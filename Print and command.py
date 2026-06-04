741601b12a612d7c940b8ab09212f41d83faa9d0
akshitha3006 <akshithadani@gmail.com>
2026-06-04 17:50:51 +0530

    Add daily routine print statements

---
+++ Print and command.py
@@ -0,0 +1,11 @@
+# My daily routine
+
+print("Wake up at 7 AM")
+print("Have breakfast")
+print("Go to work")
+print("Work for 8 hours")
+print("Come home")
+print("Have dinner")
+print("Relax for 2 hours")
+print("Go to bed at 10 PM")
+print("Total number of classes this month is:",4*3)
