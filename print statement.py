3fefee7eccde919fc5ef46cf974d29d280df5f5f
akshitha3006 <akshithadani@gmail.com>
2026-06-07 17:25:45 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,6 @@
+name= "Akshitha"
+print("This is about me \n")
+print("Hello, My name is", name)
+print("I am in grade ",11)
+print("I like to learn python",end=" ")
+print("and I am excited to learn more about it")
