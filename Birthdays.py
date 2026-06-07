1175c804012ffa7e7e569e4eecc577d75459d1f4
akshitha3006 <akshithadani@gmail.com>
2026-06-07 18:13:58 +0530

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,16 @@
+# Storing birthdays of Ravi's friends using variables
+frnd1_name = "Zayn"
+frnd1_birthday = "5th June"
+frnd2_name = "Tara"
+frnd2_birthday = "12th August"
+frnd3_name = "Nithin"
+frnd3_birthday = "6th July"
+frnd4_name = "Jagan"
+frnd4_birthday = "15th September"
+frnd5_name = "Kaushika"
+frnd5_birthday = "20th December"
+print(frnd1_name + "'s birthday is on " + frnd1_birthday)
+print(frnd2_name + "'s birthday is on " + frnd2_birthday)
+print(frnd3_name + "'s birthday is on " + frnd3_birthday)
+print(frnd4_name + "'s birthday is on " + frnd4_birthday)
+print(frnd5_name + "'s birthday is on " + frnd5_birthday)
