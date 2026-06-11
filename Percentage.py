7bb4607f00fd2a66f942bc16993f95e8293776c1
akshitha3006 <akshithadani@gmail.com>
2026-06-11 17:34:35 +0530

    Add files via upload

---
+++ Percentage.py
@@ -0,0 +1,8 @@
+maths = 40
+science = 70 
+hindi = 50
+english = 60
+
+total_marks= (maths + science + hindi + english)
+percentage = (total_marks/4)
+print("the percentage of the student is", percentage)
