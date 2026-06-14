57c6c5ab6216e283bf26ca2130063be595f2086f
akshitha3006 <akshithadani@gmail.com>
2026-06-14 17:19:44 +0530

    Add files via upload

---
+++ Conditional statement.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number:"))
+if num>0:
+    print("The number",num," is positive")
+elif num==0:
+    print("The number is zero")  
+else:
+    print("The number",num," is negative")    
