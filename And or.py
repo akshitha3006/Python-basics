2b627972a7574bae9196ccf5f66c9aadb8a8eada
akshitha3006 <akshithadani@gmail.com>
2026-06-16 17:27:51 +0530

    Add files via upload

---
+++ And or.py
@@ -0,0 +1,15 @@
+a= -6
+b= 27
+c= 36
+
+if c>=a and c>=b:
+    print(c," is the greatest number")
+elif b>=a and b>=c:
+    print(b,"is the greatest number" )
+else:
+     print(a,"is the greatest number")  
+
+if  a>=0 or b>=0:  
+    print("either is greater than zero")
+else:
+    print("No number is greater than zero")      
