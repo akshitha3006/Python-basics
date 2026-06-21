4be150d36953b86653386e11f76e35fc20846ac1
akshitha3006 <akshithadani@gmail.com>
2026-06-21 17:30:14 +0530

    Add files via upload

---
+++ Mean value.py
@@ -0,0 +1,8 @@
+mean = 38
+sum_of_40_num= 40*38
+misread_num = 36
+correct_num = 56
+difference = correct_num - misread_num
+correct_sum = sum_of_40_num + difference
+correct_mean = correct_sum/40
+print("The correct mean value is:",correct_mean)
