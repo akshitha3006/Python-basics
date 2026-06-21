964cbedd3cbbcd680ad947d63f71184866f51d90
akshitha3006 <akshithadani@gmail.com>
2026-06-21 17:19:21 +0530

    Add files via upload

---
+++ Divisible num.py
@@ -0,0 +1,6 @@
+numerator = int(input("Enter the first number:"))
+denominator = int(input("Enter the secong number:"))
+if numerator%denominator==0:
+    print("the number is divisible")
+else:
+    print("The number is not divisible")    
