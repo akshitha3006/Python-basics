ea72c016e1147012b441076e1804501b372e580c
akshitha3006 <akshithadani@gmail.com>
2026-06-23 18:45:59 +0530

    Add files via upload

---
+++ Ride selection.py
@@ -0,0 +1,24 @@
+print("Please select a ride between Bike and Car:")
+vehicle = input("Enter your choice:")
+if vehicle == "Bike":
+    print("You have selected Bike")
+    if vehicle == "Bike":
+        print("Please select the type of bike you want to ride between sports bike and dirt bike:")
+        bike_type = input("Enter your choice:")
+        if bike_type == "Sports Bike":
+            print("You have selected Sports Bike")
+        else: 
+            print("You have selected Dirt Bike")    
+        
+           
+else:
+    print("You have selected Car")
+    if vehicle == "Car":
+        print("Please select the type of car you want to ride between Defender and Mitsubishi:")
+        car_type = input("Enter your choice:")
+        if car_type == "Defender":
+            print("You have selected Defender")
+        else: 
+            print("You have selected Mitsubishi")
+        
+     
