4d849a260a66e1d66147f4985ad7591d0b654967
akshitha3006 <akshithadani@gmail.com>
2026-07-12 10:15:16 +0530

    Add files via upload

---
+++ Inventory stock counter.py
@@ -0,0 +1,9 @@
+item_count =1
+while item_count<=2:
+    print("Starting to pack item",item_count)
+    box_count= 1
+    while box_count<=3:
+        print("Packing box",box_count,"of item",item_count)
+        box_count+=1
+    item_count = item_count +1
+    print()    
