79d684cdcb0ec34615697a6fe55cd894cbdc1ba0
akshitha3006 <akshithadani@gmail.com>
2026-08-14 17:34:39 +0530

    Add files via upload

---
+++ Play with lists.py
@@ -0,0 +1,9 @@
+list=[13,30,6,7,27,10]
+count=0
+sum=sum(list)
+average=sum/len(list)
+list.sort()
+print("The sum of the list is:", sum)
+print("The average of the list is:", average)
+print("The smallest number in the list is:", list[0])
+print("The largest number in the list is:", list[-1])
