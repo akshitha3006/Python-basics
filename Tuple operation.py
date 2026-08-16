a4eb1bd589568822e15b33cb27c50c8f81ba7c11
akshitha3006 <akshithadani@gmail.com>
2026-08-16 09:18:50 +0530

    Add files via upload

---
+++ Tuple operation.py
@@ -0,0 +1,11 @@
+tuple=(13,"Ashitha",True,3.14)
+print(tuple)
+
+tuple1=(13,30,6,7,27,10)
+print(tuple1)
+tuple2=tuple1+(9,)
+print(tuple2)
+
+tuple3=("Dinosaur","Tiger","Chocolate","Brownie")
+print(len(tuple3))
+print(tuple3[0])
