2f3ab52df3d27a253f07c4019bab1144a7c46b93
akshitha3006 <akshithadani@gmail.com>
2026-08-21 17:40:45 +0530

    Add files via upload

---
+++ Country code.py
@@ -0,0 +1,4 @@
+code={"India":'0091',"Australia":'0025',"Nepal":'00977'}
+print("Country code for India:" + code.get("India"))
+print("Country code for Australia:" + code.get("Australia"))
+print("Country code for Nepal:" + code.get("Nepal"))
